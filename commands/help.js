7a8aab3367dac92511f35669bc1268fba5522adc
mohammadparvez921 <parrymd786@gmail.com>
2022-05-29 13:00:53 +0530

    project cmopleted

--- commands/help.js
+++ commands/help.js
@@ -1,19 +1,17 @@
 function help() {
     console.log(`
         These are some myCLI commands used in various situations:
+
             1. node main.js tree <path>
             2. node main.js organize <path>
             3. node main.js help
     `);
 }
 
-// function abc() {
-//     console.log("in help.js");
-// }
+
 
 module.exports = {
-   //key value
+    //key value
     help: help,
-    // haathi:help
-    // ghoda:abc
+    
 }
